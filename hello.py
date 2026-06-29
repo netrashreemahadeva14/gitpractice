effdd4024bea8bd15548bdf0aaa21a5d49f4a644
Netrashree Mahadeva <netrashree.mahadeva@gmail.com>
2026-06-29 08:58:12 +0530

    Python

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello, World!")
-name = "Harshil Baby"
+name = input("What is your name? ") 
+#Everything in input function is treated as a string. So, if you enter a number, it will be treated as a string.
 print(f"Nice to meet you, {name}!")
