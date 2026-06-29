6e917d686f1a7664bb020d4edb0dc4129d9df946
Netrashree Mahadeva <netrashree.mahadeva@gmail.com>
2026-06-29 10:41:44 +0530

    Python learning

--- hello.py
+++
@@ -1,4 +0,0 @@
-print("Hello, World!")
-name = input("What is your name? ") 
-#Everything in input function is treated as a string. So, if you enter a number, it will be treated as a string.
-print(f"Nice to meet you, {name}!")
--- variables.py
+++
@@ -1,6 +0,0 @@
-#variable should always start with letter or _. It cannot start with numbers or special characters. Variable names are case-sensitive.  
-#variable names can contain letters, numbers, and underscores.
-#variable names cannot contain spaces or special characters (except for underscores).
-#Variable name should not be a reserved keyword in Python.
-#variable name should not have spaces in between. It should be a single word or multiple words joined together without spaces.
-#Follow python naming conventions (PEP 8) for variable names. Use lowercase letters and separate words with underscores for better readability.Ex: Student_name, student_age, student_address, etc. 
